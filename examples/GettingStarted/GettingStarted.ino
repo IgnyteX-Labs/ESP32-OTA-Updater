6ad34aba35cb1416cc5635c7dd328e05f5a23401
Veit Auckenthaler <64910316+toqix@users.noreply.github.com>
2025-02-25 18:39:37 +0100

    feat: Added GettingStarted Example

---
+++ examples/GettingStarted/GettingStarted.ino
@@ -0,0 +1,31 @@
+#include <Arduino.h> // Remove this import when using Arduino IDE
+
+#include <ESP32_OTA_Updater.h>
+
+// Define the github repository to download the update from
+#define OWNER "github_username"
+#define REPO "github_repo"
+#define FIRMWARE "firmware.bin"
+// (Optional)
+#define GITHUB_ACCESS_TOKEN "finegrained_gh_token" 
+
+// Initialize the OTA Updater (Note that the GIT_TAG property has to be set in the pio build flags in platformio.ini (look at the README example for how to do that)) 
+ESP32_OTA_Updater ota(OWNER, REPO, FIRMWARE, GIT_TAG, GITHUB_ACCESS_TOKEN);
+void setup() {
+    // Setup your code
+}
+
+void loop() {
+    // This has to be run repeatedly (the library uses millis internally to not make an api call to often...)
+    if(ota.available()) {
+        // Try to install the available update
+        if(!ota.downloadAndInstall()) {
+            Serial.printf("An error occurred when trying to install an ota update: Error Code: %d, Description: %s\n", ota.getErrorCode(), ota.getErrorDescription());
+        }
+        // The update was successfully downloaded and preprared for installation:
+        
+        // PUT ANY WORK THAT SHOULD BE DONE BEFORE REBOOTING HERE!
+
+        ota.reboot();
+    }
+}
