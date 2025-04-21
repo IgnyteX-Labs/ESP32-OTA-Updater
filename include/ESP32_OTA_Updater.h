8de22cfbace30de7457dd34070dbb21fb4eb038d
Veit Auckenthaler <64910316+toqix@users.noreply.github.com>
2025-04-21 10:06:54 +0200

    feat: Implemented Update functionality

--- include/ESP32_OTA_Updater.h
+++ include/ESP32_OTA_Updater.h
@@ -7,90 +7,101 @@
 
 #include <WString.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <HTTPClient.h>
 
 #include "Errors.h"
 #include "SemanticVersion.h"
 
-#define ESP32_OTA_UPDATER_SHORTSTRING_LENGTH 25
-#define ESP32_OTA_UPDATER_LONGSTRING_LENGTH 50
-
-#ifndef ESP32_OTA_UPDATER_FORMAT_LITTLEFS_IF_FAILED
-#define ESP32_OTA_UPDATER_FORMAT_LITTLEFS_IF_FAILED true
-#endif
+#define ESP32_OTA_UPDATER_SHORTSTRING_LENGTH 50
+#define ESP32_OTA_UPDATER_LONGSTRING_LENGTH 150
 
 /**
  * @class ESP32_OTA_Updater
  * @brief Class for performing Over-The-Air (OTA) updates on ESP32 devices, with Github Actions and Releases.
- * 
+ *
  * This class checks for new Github Releases compares the semantic version and downloads/installs the firmware update. For now the System automatically uses the LittleFS file system, but a custom fs:FS can be specified on begin().
- * 
+ *
  */
 class ESP32_OTA_Updater
 {
 private:
-    char repositry_owner[ESP32_OTA_UPDATER_SHORTSTRING_LENGTH]; /**< The owner of the repository where the firmware is build an released. */
-    char repositry_name[ESP32_OTA_UPDATER_SHORTSTRING_LENGTH]; /**< The name of the repository where the firmware is build an released. */
-    char gh_api_key[ESP32_OTA_UPDATER_LONGSTRING_LENGTH]; /**< (Fine Grained) Github Personal Access Token. Required for private repositries! */
-    bool api_key_defined; /**< True if the Github API key is defined, false otherwise. */
+    char repositry_owner[ESP32_OTA_UPDATER_SHORTSTRING_LENGTH];     /**< The owner of the repository where the firmware is build an released. */
+    char repositry_name[ESP32_OTA_UPDATER_SHORTSTRING_LENGTH];      /**< The name of the repository where the firmware is build an released. */
+    char gh_api_key[ESP32_OTA_UPDATER_LONGSTRING_LENGTH];           /**< (Fine Grained) Github Personal Access Token. Required for private repositries! */
+    bool api_key_defined;                                           /**< True if the Github API key is defined, false otherwise. */
     char firmware_asset_path[ESP32_OTA_UPDATER_SHORTSTRING_LENGTH]; /**< The path to the firmware binary file on the Github Release -> the asset name. */
-    
-    bool new_version_available = false; /**< True if a new firmware version is available, false otherwise. */
-    Version new_version = NULL; /**< The new semantic version of the firmware. */
-    char binary_download_url[ESP32_OTA_UPDATER_SHORTSTRING_LENGTH]; /**< The URL to download the firmware binary file. */
-    int binary_size = 0; /**< The size of the firmware binary file. */
+    const char *http_useragent = "ESP32-OTA-Updater";
 
-    Version current_version; /**< The current semantic version of the firmware. */
-    ESP32_OTA_Updater_Error error; /**< The error status of the OTA updater. */
-    WiFiClientSecure *wifi_client_secure; /**< The WifiClientSecure object for HTTPS communication. */
-    HTTPClient http_client; /**< The HTTPClient object for making HTTP requests. */
+    bool new_version_available = false;                            /**< True if a new firmware version is available, false otherwise. */
+    Version new_version = NULL;                                    /**< The new semantic version of the firmware. */
+    char binary_download_url[ESP32_OTA_UPDATER_LONGSTRING_LENGTH]; /**< The URL to download the firmware binary file. */
+    int binary_size = 0;                                           /**< The size of the firmware binary file. */
+
+    Version current_version;             /**< The current semantic version of the firmware. */
+    ESP32_OTA_Updater_Error error;       /**< The error status of the OTA updater. */
+    WiFiClientSecure wifi_client_secure; /**< The WifiClientSecure object for HTTPS communication. */
+    HTTPClient http_client;              /**< The HTTPClient object for making HTTP requests. */
+
+    const char *rootCert;
+
+    void updateProgressCallback(size_t progress, size_t size);
+    int httpclientSendRequest(HTTPClient &http_client);
+    inline void _begin(const char *owner, const char *repo, const char *firmware_path, const char *current_version);
 
 public:
     /**
-     * @brief Constructs an instance of the ESP32_OTA_Updater class.
+     * @brief Constructor for the ESP32_OTA_Updater class.
+     *
+     * This constructor initializes the OTA updater with the specified root certificates
+     * for secure HTTPS communication.
+     *
+     * @note Please supply the all Certs for all Github endpoints concatenated in one string
+     *
+     * @param rootCertificate The root certificates for GitHub's HTTPS server and Githubs Download Server.
      */
-    ESP32_OTA_Updater();
-    
+    ESP32_OTA_Updater(const char *rootCertificate);
+
     /**
-     * @brief Initializes the ESP32 OTA Updater. 
-     * 
+     * @brief Initializes the ESP32 OTA Updater.
+     *
      * @param owner The owner of the repository where the firmware is build an released.
      * @param repo The name of the repository where the firmware is build an released.
      * @param firmware_path The path to the firmware binary file on the Github Release -> the asset name.
      * @param current_version The current version of the firmware.
      * @param gh_api_key The (Fine Grained) Github Personal Access Token.
-     * 
+     *
      * @return true if the initialization was successful, false otherwise.
      */
-    bool begin(const char owner[], const char repo[], const char firmware_path[], const char current_version[], const char gh_api_key[]);
+    bool begin(const char *owner, const char *repo, const char *firmware_path, const char *current_version, const char *api_key);
 
     /**
-     * @brief Initializes the ESP32 OTA Updater. 
-     * 
+     * @brief Initializes the ESP32 OTA Updater.
+     *
      * @param owner The owner of the repository where the firmware is build an released.
      * @param repo The name of the repository where the firmware is build an released.
      * @param firmware_path The path to the firmware binary file on the Github Release -> the asset name.
      * @param current_version The current version of the firmware.
-     * 
+     *
      * @return true if the initialization was successful, false otherwise.
      */
-    bool begin(const char owner[], const char repo[], const char firmware_path[], const char current_version[]);
+    bool begin(const char *owner, const char *repo, const char *firmware_path, const char *current_version);
 
     /**
      * @brief Checks if a firmware update is available.
-     * 
+     *
      * This function checks if a new firmware update is available by comparing the current version with the version
      * specified in the firmware update file.
-     * 
+     *
      * @return True if a firmware update is available, false otherwise.
      */
     bool available();
 
     /**
      * @brief Downloads and installs the firmware update. Use 'reboot()' for the esp to reboot and the update to take effect!
-     * 
+     *
      * This function downloads the firmware update file from the specified URL and installs it on the ESP32 device.
-     * 
+     *
      * @note This function should only be called if a firmware update is available (i.e., `available()` returns true).
      */
     bool downloadAndInstall();
--- src/ESP32_OTA_Updater.cpp
+++ src/ESP32_OTA_Updater.cpp
@@ -2,178 +2,220 @@
 #include <ArduinoJson.h>
 #include <Update.h>
 
-ESP32_OTA_Updater::ESP32_OTA_Updater() {
+ESP32_OTA_Updater::ESP32_OTA_Updater(const char *rootCertificate)
+{
     error = ESP32_OTA_Updater_Error::NOT_INITIALIZED;
+    rootCert = rootCertificate;
 }
 
-bool ESP32_OTA_Updater::begin(const char owner[], const char repo[], const char firmware_path[], const char current_version[], const char api_key[]) {
-   strncpy(repositry_owner, owner, ESP32_OTA_UPDATER_SHORTSTRING_LENGTH);
-    strncpy(repositry_name, repo, ESP32_OTA_UPDATER_SHORTSTRING_LENGTH);
-    strncpy(firmware_asset_path, firmware_path, ESP32_OTA_UPDATER_SHORTSTRING_LENGTH);
-    wifi_client_secure = new WiFiClientSecure();
+bool ESP32_OTA_Updater::begin(const char *owner, const char *repo, const char *firmware_path, const char *current_version, const char *api_key)
+{
+    _begin(owner, repo, firmware_path, current_version);
+
     strncpy(gh_api_key, api_key, ESP32_OTA_UPDATER_LONGSTRING_LENGTH);
-    api_key_defined = true;    
+    api_key_defined = true;
 
     error = ESP32_OTA_Updater_Error::NO_ERROR;
+    return true;
 }
 
-bool ESP32_OTA_Updater::begin(const char owner[], const char repo[], const char firmware_path[], const char current_version[]) {
+bool ESP32_OTA_Updater::begin(const char *owner, const char *repo, const char *firmware_path, const char *current_version)
+{
+    _begin(owner, repo, firmware_path, current_version);
+
+    api_key_defined = false;
+
+    error = ESP32_OTA_Updater_Error::NO_ERROR;
+    return true;
+}
+
+inline void ESP32_OTA_Updater::_begin(const char *owner, const char *repo, const char *firmware_path, const char *current_version)
+{
     strncpy(repositry_owner, owner, ESP32_OTA_UPDATER_SHORTSTRING_LENGTH);
     strncpy(repositry_name, repo, ESP32_OTA_UPDATER_SHORTSTRING_LENGTH);
     strncpy(firmware_asset_path, firmware_path, ESP32_OTA_UPDATER_SHORTSTRING_LENGTH);
-    wifi_client_secure = new WiFiClientSecure();
-    api_key_defined = false;
-    
-    error = ESP32_OTA_Updater_Error::NO_ERROR;
+    this->current_version = Version(current_version);
+
+    wifi_client_secure.setCACert(rootCert);
+
+    Update.onProgress(std::bind(&ESP32_OTA_Updater::updateProgressCallback, this, std::placeholders::_1, std::placeholders::_2));
+}
+
+void ESP32_OTA_Updater::updateProgressCallback(size_t progress, size_t size)
+{
+    // Update Progress
+    Serial.printf("Installing %d of %d bytes.\n", progress, size);
+}
+
+int ESP32_OTA_Updater::httpclientSendRequest(HTTPClient &http_client)
+{
+    http_client.useHTTP10(true); // use HTTP/1.0 for update since the update handler not support any transfer Encoding
+    http_client.setTimeout(15000);
+    http_client.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    http_client.setUserAgent(http_useragent);
+
+    // Add Authorization Header for Github API if defined.
+    if (api_key_defined)
+    {
+        http_client.setAuthorizationType("Bearer");
+        http_client.setAuthorization(gh_api_key);
+    }
+
+    http_client.addHeader("X-GitHub-Api-Version", "2022-11-28"); // Set Github Api Version
+
+    int code = http_client.GET();
+    int len = http_client.getSize();
+
+    if (code == HTTP_CODE_OK)
+    {
+        if (len <= 0)
+        {
+            error = OTA_RESPONSE_INVALID;
+            return 0;
+        }
+        return len; // return the length of the response
+    }
+    else if (code < 0)
+    {
+        error = ESP32_OTA_Updater_Error::WIFI_NOT_CONNECTED;
+        return code; // Return a negative value to indicate failure
+    }
+    else
+    {
+        error = ESP32_OTA_Updater_Error::OTA_NOT_AVAILABLE;
+        return -code; // Return (minus) the response code to indicate failure although a successful request was made
+    }
 }
 
 bool ESP32_OTA_Updater::available()
 {
-    if(error != ESP32_OTA_Updater_Error::NO_ERROR)
+    /*
+     * TODO: Implement a millis nonblocking return false to not always perform an http request
+     */
+
+    if (error != ESP32_OTA_Updater_Error::NO_ERROR)
     {
         return false;
     }
 
     // Check if a firmware update is available
-    char url[50+strlen(repositry_owner)+strlen(repositry_name)];
-    sprintf(url, "https://api.github.com/repos/%s/%s/releases/latest", repositry_owner, repositry_name);
-    
-    if(!http_client.begin(*wifi_client_secure, url))
+    const size_t urlLen = 50 + strlen(repositry_owner) + strlen(repositry_name);
+    char url[urlLen];
+    snprintf(url, urlLen, "https://api.github.com/repos/%s/%s/releases/latest", repositry_owner, repositry_name);
+
+    if (!http_client.begin(wifi_client_secure, url))
     {
         error = ESP32_OTA_Updater_Error::OTA_NOT_AVAILABLE;
         return false;
     }
     http_client.addHeader("Accept", "application/vnd.github+json");
-    if(api_key_defined)
-    {
-        http_client.addHeader("Authorization: Bearer %s", gh_api_key);
-    }
-    http_client.addHeader("X-GitHub-Api-Version", "2022-11-28");
-    http_client.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-    
-    int http_code = http_client.GET();
 
-    if(http_code != HTTP_CODE_OK)
+    if (httpclientSendRequest(http_client) <= 0)
     {
-        if(http_code < 0)
-        {
-            error = ESP32_OTA_Updater_Error::WIFI_NOT_CONNECTED;
-        }
-        else
-        {
-            error = ESP32_OTA_Updater_Error::OTA_NOT_AVAILABLE;
-        }
-        return false;
+        return false; // Error Codes are set in the method itself
     }
 
+    // ***** Response Decoding and Handling ******
     JsonDocument doc;
 
     DeserializationError json_error = deserializeJson(doc, http_client.getStream());
     http_client.end();
-    if(json_error)
+    if (json_error)
     {
         error = ESP32_OTA_Updater_Error::OTA_FAILED_TO_DESERIALIZE;
         return false;
     }
 
     // Check version from the JSON response
-    if(!doc.containsKey("tag_name")) {
+    if (!doc.containsKey("tag_name"))
+    {
         error = ESP32_OTA_Updater_Error::OTA_RESPONSE_INVALID;
         return false;
     }
-    Version latest_version(Version(doc["tag_name"].as<const char*>()));
-    
-    if(latest_version <= current_version) {
+    Version latest_version(Version(doc["tag_name"].as<const char *>()));
+
+    if (latest_version <= current_version)
+    {
         return false;
     }
     // Find the asset with the binary:
-    if(!doc.containsKey("assets"))
+    if (!doc.containsKey("assets"))
     {
         error = ESP32_OTA_Updater_Error::OTA_RESPONSE_INVALID;
         return false;
     }
     JsonArray assets = doc["assets"].as<JsonArray>();
     bool assetFound = false;
-    for(JsonObject jsonAsset: assets) {
-        if(jsonAsset.containsKey("name") && strcmp(jsonAsset["name"].as<const char*>(), firmware_asset_path) == 0)
+    for (JsonObject jsonAsset : assets)
+    {
+        if (jsonAsset.containsKey("name") && strcmp(jsonAsset["name"].as<const char *>(), firmware_asset_path) == 0)
         {
             // Found the download URL
-            strncpy(binary_download_url, jsonAsset["url"].as<const char*>(), ESP32_OTA_UPDATER_LONGSTRING_LENGTH);
+            strncpy(binary_download_url, jsonAsset["url"].as<const char *>(), ESP32_OTA_UPDATER_LONGSTRING_LENGTH);
             binary_size = jsonAsset["size"].as<int>();
-            
+
             new_version_available = true;
             new_version = latest_version;
             assetFound = true;
             break;
         }
     }
-    if(!assetFound)
+    if (!assetFound)
     {
         error = ESP32_OTA_Updater_Error::OTA_RESPONSE_INVALID;
         return false;
     }
-    
-   return true;
+
+    return true;
 }
 
 bool ESP32_OTA_Updater::downloadAndInstall()
 {
-    if(error != ESP32_OTA_Updater_Error::NO_ERROR)
+    if (error != ESP32_OTA_Updater_Error::NO_ERROR)
     {
         return false;
     }
 
     // Download the firmware from the URL
-    if(!http_client.begin(*wifi_client_secure, binary_download_url))
+    if (!http_client.begin(wifi_client_secure, binary_download_url))
     {
         error = ESP32_OTA_Updater_Error::OTA_DOWNLOAD_FAILED;
         return false;
     }
-
     http_client.addHeader("Accept", "application/octet-stream");
-    if(api_key_defined)
-    {
-        http_client.addHeader("Authorization: Bearer %s", gh_api_key);
-    }
-    http_client.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    http_client.addHeader("Cache-Control", "no-cache");
 
-    int http_code = http_client.GET();
-
-    if(http_code != HTTP_CODE_OK)
+    int update_size = httpclientSendRequest(http_client);
+    if (update_size <= 0)
     {
-        if(http_code < 0)
-        {
-            error = ESP32_OTA_Updater_Error::WIFI_NOT_CONNECTED;
-        }
-        else
-        {
-            error = ESP32_OTA_Updater_Error::OTA_NOT_AVAILABLE;
-        }
-        return false;
+        return false; // Error codes are set in the method itself!
     }
+
+    // Update of size update_size is ready for download
+
     // Directly write the download stream to the file stream...
-    int update_size = http_client.getSize();
+    NetworkClient *client = http_client.getStreamPtr();
 
-    if (!Update.begin(update_size)) {
+    if (!Update.begin(update_size, U_FLASH))
+    {
         error = ESP32_OTA_Updater_Error::OTA_INSTALL_FAILED;
         http_client.end();
         return false;
     }
 
-    if (Update.writeStream(http_client.getStream()) != update_size) {
-        error = ESP32_OTA_Updater_Error::OTA_INSTALL_FAILED;
-        Update.end();
-        http_client.end();
-        return false;
-    }
+    /*
+     * TODO: Implemente Crypto at this point, to allow for encrypted firmware binaries.
+     */
 
-    if (!Update.end(true)) {
+    if (Update.writeStream(*client) != update_size)
+    {
         error = ESP32_OTA_Updater_Error::OTA_INSTALL_FAILED;
         http_client.end();
         return false;
     }
 
-    if (!Update.isFinished()) {
+    if (!Update.end())
+    {
         error = ESP32_OTA_Updater_Error::OTA_INSTALL_FAILED;
         http_client.end();
         return false;
@@ -184,7 +226,8 @@ bool ESP32_OTA_Updater::downloadAndInstall()
     return true;
 }
 
-void ESP32_OTA_Updater::reboot() {
+void ESP32_OTA_Updater::reboot()
+{
     ESP.restart();
 }
 
@@ -195,25 +238,25 @@ ESP32_OTA_Updater_Error ESP32_OTA_Updater::getErrorCode()
 
 String ESP32_OTA_Updater::getErrorDescription()
 {
-    switch(error)
+    switch (error)
     {
-        case ESP32_OTA_Updater_Error::NO_ERROR:
-            return "No error";
-        case ESP32_OTA_Updater_Error::WIFI_NOT_CONNECTED:
-            return "Wifi not connected";
-        case ESP32_OTA_Updater_Error::NOT_INITIALIZED:
-            return "Not initialized";
-        case ESP32_OTA_Updater_Error::OTA_NOT_AVAILABLE:
-            return "OTA Update not available, please configure the git repo!";
-        case ESP32_OTA_Updater_Error::OTA_DOWNLOAD_FAILED:
-            return "OTA download failed";
-        case ESP32_OTA_Updater_Error::OTA_INSTALL_FAILED:
-            return "OTA install failed";
-        case ESP32_OTA_Updater_Error::OTA_FAILED_TO_DESERIALIZE:
-            return "OTA failed to deserialize";
-        case ESP32_OTA_Updater_Error::OTA_RESPONSE_INVALID:
-            return "OTA response invalid";
-        default:
-            return "Unknown error";
+    case ESP32_OTA_Updater_Error::NO_ERROR:
+        return F("No error");
+    case ESP32_OTA_Updater_Error::WIFI_NOT_CONNECTED:
+        return F("Wifi not connected");
+    case ESP32_OTA_Updater_Error::NOT_INITIALIZED:
+        return F("Not initialized");
+    case ESP32_OTA_Updater_Error::OTA_NOT_AVAILABLE:
+        return F("OTA Update not available, please configure the git repo!");
+    case ESP32_OTA_Updater_Error::OTA_DOWNLOAD_FAILED:
+        return F("OTA download failed");
+    case ESP32_OTA_Updater_Error::OTA_INSTALL_FAILED:
+        return F("OTA install failed");
+    case ESP32_OTA_Updater_Error::OTA_FAILED_TO_DESERIALIZE:
+        return F("OTA failed to deserialize");
+    case ESP32_OTA_Updater_Error::OTA_RESPONSE_INVALID:
+        return F("OTA response invalid");
+    default:
+        return F("Unknown error");
     }
 }
